f0eceb3727e8413f844400bd566d84f3d2aee5c1
Diego Rojas <ale.rgdap@gmail.com>
2023-10-09 15:34:37 -0600

    code

--- merge.py
+++ merge.py
@@ -1,6 +1,28 @@
-class Solution:
-    def mergeAlternately(self, word1: str, word2: str) -> str:
-        pass
+def mergeAlternately(word1: str, word2: str) -> str:
+    word = ""
+    len1 = len(word1)
+    len2 = len(word2)
+    if len1 > len2:
+        for i in range(len1):
+            print(word1[i])
+            word += word1[i]
+            if i > len2 or i == len2:
+                i+=1
+            else:
+                print(word2[i])
+                word += word2[i]
+    else:
+        for i in range(len2):
+            if i < len1 or i == len1-1:
+                print(word1[i])
+                word +=word1[i]
+            print(word2[i])
+            word += word2[i]
+    return word
+
+
 x = input("word: ")
-y = input("word: ")
+y = input("word2: ")
+word = mergeAlternately(x, y)
+print(word)
 
